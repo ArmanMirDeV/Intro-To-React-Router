c18bc8f0b1788e45bda035420057594f527428bd
Arman Mir <mirarman8583@gmail.com>
2025-09-30 11:03:59 +0600

    Understanding nester routing ,Outlet, Link and NavLink

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router';
+import Footer from '../Footer/Footer';
+import SideBar from '../SideBar/SideBar';
+import './Root.css'
 
 const Root = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            {/* <Outlet></Outlet> */}
+
+            <div className='root-main' >
+               <SideBar></SideBar>
+                <Outlet></Outlet>
+            </div>
+            
+            <Footer></Footer>
         </div>
     );
 };
